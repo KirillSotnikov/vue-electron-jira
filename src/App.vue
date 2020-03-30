4067de5ba79032eca3a96b94f3167855bd413bc6
Екатерина Цховребашвили <sotnikov_k@outlook.com>
2020-03-30 21:37:04 +0300

    init project

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,55 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    
+    <app-drawer
+      :items="drawerList"
+      :drawerShow="drawerShow"
+    />
+
+    <app-navbar
+      :toggleDrawer="toggleDrawer"
+    />
+
+    <!-- Sizes your content based upon application components -->
+    <v-content>
+
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+
+        <!-- If using vue-router -->
+        <router-view></router-view>
+      </v-container>
+    </v-content>
+
+    <v-footer app>
+      <!-- -->
+    </v-footer>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Drawer from './Components/Layout/Drawer.vue';
+import Navbar from './Components/Layout/Navbar.vue';
 
-#nav {
-  padding: 30px;
+export default {
+  name: 'App',
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+  components: {
+    'app-drawer': Drawer,
+    'app-navbar': Navbar
+  },
 
-    &.router-link-exact-active {
-      color: #42b983;
+  data: () => ({
+    drawerShow: false,
+    drawerList: [
+      { title: 'Home', icon: 'mdi-account-box' }
+    ],
+  }),
+
+  methods: {
+    toggleDrawer () {
+      this.drawerShow = !this.drawerShow
     }
   }
-}
-</style>
+};
+</script>
---
+++ src/Components/Layout/Navbar.vue
@@ -0,0 +1,44 @@
+<template>
+    <v-app-bar hide-on-scroll app>
+      <v-app-bar-nav-icon @click="toggleDrawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Jira №2</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-menu
+        left
+        bottom
+      >
+        <template v-slot:activator="{ on }">
+          <v-btn icon v-on="on">
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item
+            v-for="n in 5"
+            :key="n"
+            @click="() => {}"
+          >
+            <v-list-item-title>Option {{ n }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+    props: {
+        toggleDrawer: {
+            type: Function
+        }
+    }
+}
+</script>
